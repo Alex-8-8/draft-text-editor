4e9f8450508b1c80a21dc833f578326514a15937
Alex-8-8 <46635183+Alex-8-8@users.noreply.github.com>
2020-04-09 19:57:23 +0300

    Updates

---
+++ precache-manifest.2d00520710499011a5a3b93dbf577a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e37688291ea46b2413fd9e5cc9e15e",
+    "url": "/draft-text-editor/index.html"
+  },
+  {
+    "revision": "4998918ff5168db94210",
+    "url": "/draft-text-editor/static/css/main.00d3cad2.chunk.css"
+  },
+  {
+    "revision": "bb2de7cf755803359e45",
+    "url": "/draft-text-editor/static/js/2.abad83e4.chunk.js"
+  },
+  {
+    "revision": "c73ba58bb12901cd9f1eaa363ae40055",
+    "url": "/draft-text-editor/static/js/2.abad83e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4998918ff5168db94210",
+    "url": "/draft-text-editor/static/js/main.4045c181.chunk.js"
+  },
+  {
+    "revision": "e31931bfe3bc465bd44c",
+    "url": "/draft-text-editor/static/js/runtime-main.f82b4d88.js"
+  }
+]);
